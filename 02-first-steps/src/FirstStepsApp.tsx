52293db60348550289ee2cc4fcadae73d91ddf38
Ebbsos <128486173+Ebbsos@users.noreply.github.com>
2025-09-01 20:17:40 -0300

    dcscscs

--- 02-first-steps/src/FirstStepsApp.tsx
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -1,12 +1,47 @@
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+interface ItemInCart {
+     productName: string;
+     quantity: number;
+}
+
+const ItemsInCart: ItemInCart[] =[
+ {
+  productName: 'ELIAS',
+  quantity:1,
+ },
+ {
+  productName:'Evelyn',
+  quantity: 2,
+
+ }, 
+ {productName:'ELIAS',
+    quantity: 3
+ }
+
+]
+
 export function FirstStepsApp() {
 
     return (//regresa un objeto
-        <>
+     /*    
             <h1>Carrito de compras</h1>
             <ItemCounter name='Nintendo' quantity={1} />
             <ItemCounter name='Iphone' quantity={2} />
             <ItemCounter name='Samsung' quantity={3}/>
-        </>)
+        */
+    <>
+     
+    {
+     ItemsInCart.map(({productName, quantity})=>(
+     <ItemCounter key={productName} name={productName} quantity={quantity}/>
+
+     )
+        
+     )
+
+    }
+
+     </>
+    )
 }
